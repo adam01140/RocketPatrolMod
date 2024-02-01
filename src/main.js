0c7f7a7fee741c6e98cc813dfe2e6b6ea8826121
adam01140 <achaaban@ucsc.edu>
2024-01-31 21:43:08 -0800

    q

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+//Adam Chaabane
+//Rocket Patrol 2 The Better One
+//Took about 4 hours
+//Mods:
+
+// Display the time remaining (in seconds) on the screen (3)
+// Create a new title screen (e.g., new artwork, typography, layout) (3)
+//Implement an alternating two-player mode (5)
+//Implement a new timing/scoring mechanism that adds time to the clock for successful hits and subtracts time for misses (5)
+//Implement mouse control for player movement and left mouse click to fire (5)
+
+
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
